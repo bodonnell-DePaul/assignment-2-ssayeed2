21425e393e5df3149dd3edcefb1dfea230ae4bf6
Sara Sayeed <ssayeed2@depaul.edu>
2024-10-08 20:17:33 -0500

    Initial Layout

---
+++ sayeed_assignment2/src/Viewport.js
@@ -0,0 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroupComp from './ListGroupComp';
+import TabComp from './TabComp';
+import FormComp from './FormComp';
+function Viewport() {
+    return (
+        <Container>
+            <h1>Assignment 2: Sara Sayeed</h1>
+            <Row>
+                <Col sm={4}><FormComp /></Col>
+                <Col sm={4}><ListGroupComp /></Col>
+                <Col sm={4}><TabComp /></Col>
+            </Row>
+        </Container>
+    );
+}
+
+
+export default Viewport; 
